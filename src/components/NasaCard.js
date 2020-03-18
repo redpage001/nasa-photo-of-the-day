45cec894ec98dbb056a8884b93b7d4c35e246f60
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-03-18 12:39:52 -0700

    created files for the project and built up card framework for project

---
+++ src/components/NasaCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CardMaker = props => {
+    return (
+        <div className="cardContainer">
+            <h1>NASA Photo of the Day!</h1>
+            <img className="pictureOfTheDay" alt="random picture of space" src="props.url"/>
+        </div>
+    )
+}
+
+export default CardMaker;
